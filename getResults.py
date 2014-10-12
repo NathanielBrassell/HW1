3a19e5f4fbe451e2c7289436c89389931ecefa31
mchaiken <mirandachaiken@gmail.com>
2014-10-12 19:52:40 -0400

    did some work on who to make sure googling works with questions
    (it does)

--- getResults.py
+++ getResults.py
@@ -1,11 +1,11 @@
-import google, urllib
+import google, urllib, regex
 from bs4 import BeautifulSoup, SoupStrainer
 
 
 def search_query(question):
     question_word= (question.split(" ")[0]).lower()
     content_words=" ".join(question.split(" ")[1:])
-    g= google.search(content_words,num=5, stop=5)
+    g= google.search(content_words,num=20, stop=20)
     search_results=""
     only_p= SoupStrainer("p")
     for result in g:
@@ -13,7 +13,7 @@ def search_query(question):
         #print search_results
     response=""
     if question_word == "who":
-        response = "who"#who(search_results)
+        response = regex.findNames(search_results,content_words)
     elif question_word == "where":
         response = "where"#where(search_results)
     elif question_word == "when":
